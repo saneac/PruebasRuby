1f3524cafd2b0f54f9c6a88e3c8498e3192f8e9b
Sandy Aparicio <saparicio@web-desarrollo.kssoluciones.com>
2015-02-20 18:13:27 -0800

    Ultimas actualizaciones del proyecto blog

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+
+ # get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+ 
+
+
+  resources :articles do
+	resources :comments
+  end
+
   root 'welcome#index'
 
 
